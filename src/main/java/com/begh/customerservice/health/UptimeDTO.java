fc930309ea7e72eb66e216bd035b635819c1ee92
Lamers <gustav@henrikssons.org>
2024-04-19 13:31:25 +0200

    uptime health check

---
+++ src/main/java/com/begh/customerservice/health/UptimeDTO.java
@@ -0,0 +1,14 @@
+package com.begh.customerservice.health;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UptimeDTO {
+    private String startedAt;
+    private String upTime;
+    private Long upTimeInSeconds;
+}
